940a0d5516aac0cbe2f18379e241e190ed0e6e75
deepakjadiwal <deepak.jadiwal@switchmyloan.in>
2025-12-27 11:04:50 +0530

    design fix

--- src/pages/UTM/UtmLinkGenerate.jsx
+++ src/pages/UTM/UtmLinkGenerate.jsx
@@ -83,7 +83,7 @@ const UTMGenerator = () => {
   ========================= */
 
   return (
-    <div className="max-w-5xl mx-auto p-6">
+    <div className="mx-auto p-6">
 
       {/* Header */}
       <div className="flex items-center gap-3 mb-6">
